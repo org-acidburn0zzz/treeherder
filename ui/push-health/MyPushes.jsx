be0ee752ef0e62a6e4c0f0ff613c3c0fe1c9d4e2
Sarah Clements <sclements313@gmail.com>
2020-10-02 15:22:49 -0700

    add updateMetrics

--- ui/push-health/MyPushes.jsx
+++ ui/push-health/MyPushes.jsx
@@ -7,9 +7,7 @@ class MyPushes extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = {
-      revisions: [],
-    };
+    this.state = {};
   }
 
   componentDidMount() {
@@ -26,16 +24,24 @@ class MyPushes extends React.Component {
 
   async fetchPushes() {
     // TODO change to author to this.props.user.email
-    const options = { repo: 'try', author: 'rmaries@mozilla.com' };
+    const options = { repo: 'try', author: 'rmaries@mozilla.com', count: 3 };
     const { data, failureStatus } = await PushModel.getList(options);
 
-    if (!failureStatus && data.results) {
+    if (!failureStatus && data.results.length) {
       const revisions = data.results.map((push) => push.revision);
-      this.setState(revisions);
+      const {
+        metrics: { linting, builds, tests },
+      } = await this.updateMetrics(revisions);
     }
     // TODO notify with failure message
   }
 
+  updateMetrics = async (revisions) => {
+    const metricData = await Promise.all(
+      revisions.map((revision) => PushModel.getHealth('try', revision)),
+    );
+    console.log(metricData);
+  };
   // TODO
   // fetch pushes by author and per repo (start with try) - start with past 10 which is TH default
   // Use same logic in Health component that's used for the tabs (failed or passed)
