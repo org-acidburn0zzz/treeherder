8dd9320bf08e3437b5898d08d1a3b68eae775834
Sarah Clements <sclements313@gmail.com>
2020-10-02 14:34:57 -0700

    add notes

--- ui/push-health/MyPushes.jsx
+++ ui/push-health/MyPushes.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Container } from 'reactstrap';
 
 class MyPushes extends React.Component {
   constructor(props) {
@@ -6,9 +7,23 @@ class MyPushes extends React.Component {
 
     this.state = {};
   }
-
+  // TODO
+  // fetch pushes by author and per repo (start with try) - start with past 10 which is TH default
+  // Use same logic in Health component that's used for the tabs (failed or passed)
   render() {
-    return <div>my pushes</div>;
+    const { user } = this.props;
+    console.log(user);
+    return (
+      <Container fluid className="mt-2 mb-5 max-width-default">
+        {!user.isLoggedIn ? (
+          <h2 className="pt-5 text-center">
+            Please log in to see your Try pushes
+          </h2>
+        ) : (
+          <div>Pushes go here</div>
+        )}
+      </Container>
+    );
   }
 }
 
