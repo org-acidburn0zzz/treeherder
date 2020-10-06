0c26a637590a609a8db5afb5173fbce32265f166
Sarah Clements <sclements313@gmail.com>
2020-10-06 12:57:02 -0700

    add CommitHistory component

--- ui/push-health/MyPushes.jsx
+++ ui/push-health/MyPushes.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
 import { Container } from 'reactstrap';
+
 import { getData } from '../helpers/http';
 import { getProjectUrl, getUrlParam } from '../helpers/location';
 import { createQueryParams, pushEndpoint } from '../helpers/url';
-
 import PushModel from '../models/push';
+import RepositoryModel from '../models/repository';
+
+import CommitHistory from './CommitHistory';
+
+const defaultRepo = 'try';
 
 class MyPushes extends React.Component {
   constructor(props) {
     super(props);
 
-    this.state = { data: [] };
+    this.state = { pushMetrics: [], currentRepo: null };
   }
 
   componentDidMount() {
@@ -21,8 +26,15 @@ class MyPushes extends React.Component {
 
   componentDidUpdate(prevProps) {
     if (!prevProps.user.isLoggedIn && this.props.user.isLoggedIn) {
+      this.fetchRepo();
       this.fetchPushes();
     }
+  }
+
+  async fetchRepo() {
+    const repos = await RepositoryModel.getList();
+    const currentRepo = repos.find((repoObj) => repoObj.name === defaultRepo);
+    this.setState({ currentRepo });
   }
 
   async fetchPushes() {
@@ -36,74 +48,37 @@ class MyPushes extends React.Component {
     const { data, failureStatus } = await getData(
       getProjectUrl(
         `${pushEndpoint}health_summary/${createQueryParams(options)}`,
-        'try',
+        defaultRepo,
       ),
     );
 
     if (!failureStatus && data.length) {
-      this.setState({ data });
+      this.setState({ pushMetrics: data });
     }
     // TODO notify with failure message
   }
 
-  // TODO
-  // Look into creating a new method on the PushViewSet to query by author return a group of metrics
-  // without the jobs so as to skip fetching the pushlist first. Will need to evaluate the query time since there is
-  // not an index on author field and will need it to be limited to top 5 most recent pushes (can introduce pagination later if needed).
-  // needed data structure:
-  //  [
-  //   {
-  //     'revision': revision,
-  //     'id': push.id, ???
-  //     'result': push_result, ???
-  //     'metrics': {
-  //         'commitHistory': {
-  //             'name': 'Commit History',
-  //             'result': 'none',
-  //             'details': commit_history_details,
-  //         },
-  //         'linting': {
-  //             'name': 'Linting',
-  //             'result': lint_result,
-  //             'details': lint_failures,
-  //             'count': count
-  //         },
-  //         'tests': {
-  //             'name': 'Tests',
-  //             'result': test_result,
-  //             'details': push_health_test_failures,
-  //             'count': count
-  //         },
-  //         'builds': {
-  //             'name': 'Builds',
-  //             'result': build_result,
-  //             'details': build_failures,
-  //             'count': count
-  //         },
-  //     },
-  //     'status': push.get_status(), ???
-  // }
-  // ]
-
-  updateMetrics = async (revisions) => {
-    const metricData = await Promise.all(
-      revisions.map((revision) => PushModel.getHealth('try', revision)),
-    );
-    console.log(metricData);
-  };
-
   render() {
     const { user } = this.props;
-
+    const { pushMetrics, currentRepo } = this.state;
     return (
       <Container fluid className="mt-2 mb-5 max-width-default">
-        {!user.isLoggedIn ? (
+        {!user.isLoggedIn && (
           <h2 className="pt-5 text-center">
             Please log in to see your Try pushes
           </h2>
-        ) : (
-          <div>Pushes go here</div>
         )}
+        {currentRepo &&
+          pushMetrics.length > 0 &&
+          pushMetrics.map((push) => (
+            <div key={push.commitHistory.currentPush.revision}>
+              <CommitHistory
+                history={push.commitHistory}
+                revision={push.commitHistory.currentPush.revision}
+                currentRepo={currentRepo}
+              />
+            </div>
+          ))}
       </Container>
     );
   }
